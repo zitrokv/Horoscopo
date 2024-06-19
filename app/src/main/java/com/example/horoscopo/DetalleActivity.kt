d9e1633bc4ced8aace5461fa4a5bd2079ef1b843
zitrokv <victor.vialasc@gmail.com>
2024-06-19 14:06:55 +0200

    consultar API horoscopo

--- app/src/main/java/com/example/horoscopo/DetalleActivity.kt
+++ app/src/main/java/com/example/horoscopo/DetalleActivity.kt
@@ -3,23 +3,36 @@ package com.example.horoscopo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import javax.net.ssl.HttpsURLConnection
 
 
 class DetalleActivity : AppCompatActivity() {
     lateinit var detalleHoroscopo: Horoscopo
     lateinit var sesion: SessionManager
-
+    lateinit var textoDelDia : TextView
     lateinit var MenuItemFavorito : MenuItem
 
     var esFavorito = false
 
+
+    init {
+        textoDelDia = findViewById(R.id.textoDelDia)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
@@ -42,6 +55,13 @@ class DetalleActivity : AppCompatActivity() {
 
         //icono en menu detalle
         esFavorito = sesion.obtenerHoroscopoFavorito()?.equals(detalleHoroscopo.id) ?: false
+
+
+        //supportActionBar?.setTitle(detalleHoroscopo.nombre)
+        //supportActionBar?.setSubtitle(detalleHoroscopo.fecha)
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        getDailyHoroscope()
 
         /*ImagenFavorito.setOnClickListener{
             if (esFavorito)
@@ -104,5 +124,49 @@ class DetalleActivity : AppCompatActivity() {
         MenuItemFavorito = menu.findItem(R.id.menu_corazon)
         estableceIconoFavorito()
         return true
+    }
+
+
+    fun getDailyHoroscope() {
+        // Llamada en hilo secundario
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                // Declaramos la url
+                val url = URL("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${detalleHoroscopo.id}&day=TODAY")
+                val con = url.openConnection() as HttpsURLConnection
+                con.requestMethod = "GET"
+                val responseCode = con.responseCode
+                Log.i("HTTP", "Response Code :: $responseCode")
+
+                // Preguntamos si hubo error o no
+                if (responseCode == HttpsURLConnection.HTTP_OK) { // Ha ido bien
+                    // Metemos el cuerpo de la respuesta en un BurfferedReader
+                    val bufferedReader = BufferedReader(InputStreamReader(con.inputStream))
+                    var inputLine: String?
+                    val response = StringBuffer()
+                    while (bufferedReader.readLine().also { inputLine = it } != null) {
+                        response.append(inputLine)
+                    }
+                    bufferedReader.close()
+
+                    // Parsear JSON
+                    val json = JSONObject(response.toString())
+                    val result =  json.getJSONObject("data").getString("horoscope_data")
+
+                    // Ejecutamos en el hilo principal
+                    /*CoroutineScope(Dispatchers.Main).launch {
+
+                    }*/
+                    runOnUiThread {
+                        textoDelDia.text = result
+                    }
+
+                } else { // Hubo un error
+                    Log.w("HTTP", "Response :: Hubo un error")
+                }
+            } catch (e: Exception) {
+                Log.e("HTTP", "Response Error :: ${e.stackTraceToString()}")
+            }
+        }
     }
 }
